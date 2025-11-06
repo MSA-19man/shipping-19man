a109a3a4cfd2a717b5af33fb2aaf497d6cc23ec1
jvn1o <junok0987@naver.com>
2025-11-06 17:42:30 +0900

    feat: AiService Test Code

---
+++ ai-service/src/test/java/com/sparta/aiservice/AiAiServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sparta.aiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class AiAiServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ ai-service/src/test/java/com/sparta/aiservice/domain/model/Ai.java
@@ -0,0 +1,19 @@
+package com.sparta.aiservice.domain.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Domain: Ai")
+public class Ai {
+
+	@Test
+	@DisplayName("Ai 생성 테스트")
+	void AiCreateTest() {
+		// Given
+
+		// When
+
+		// Then
+
+	}
+}
