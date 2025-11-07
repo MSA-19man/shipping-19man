06fd135756708cb0320ce303dcacb03009301745
naminsu <naminsoo1020@naver.com>
2025-11-07 17:02:38 +0900

    feat: 업체 단건 조회를 위한 domain 영역 코드 추가
    - findById 추가

--- company-service/src/main/java/com/sparta/companyservice/domain/repository/CompanyRepository.java
+++ company-service/src/main/java/com/sparta/companyservice/domain/repository/CompanyRepository.java
@@ -1,5 +1,8 @@
 package com.sparta.companyservice.domain.repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import com.sparta.companyservice.domain.model.Company;
 
 public interface CompanyRepository {
@@ -7,4 +10,6 @@ public interface CompanyRepository {
 	Company save(Company company);
 
 	boolean existsByName(String name);
+
+	Optional<Company> findById(UUID companyId);
 }
