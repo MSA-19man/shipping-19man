adddbecaf0ee6ff70ebe45ea0d31b0218132845e
naminsu <naminsoo1020@naver.com>
2025-11-10 09:26:52 +0900

    feat: Retry, FeignClients 어노테이션 추가

--- product-service/src/main/java/com/sparta/productservice/ProductServiceApplication.java
+++ product-service/src/main/java/com/sparta/productservice/ProductServiceApplication.java
@@ -3,13 +3,17 @@ package com.sparta.productservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
+@EnableRetry
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class ProductServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(ProductServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ProductServiceApplication.class, args);
+	}
 
 }
