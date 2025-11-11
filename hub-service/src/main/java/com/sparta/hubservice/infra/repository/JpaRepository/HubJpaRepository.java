07a0d81596712701f118ebdf6e52f52cda0aca9a
soyoon <152174972+soyxni@users.noreply.github.com>
2025-11-12 00:24:27 +0900

    fix: HubJpaRepository 수정

--- hub-service/src/main/java/com/sparta/hubservice/infra/repository/JpaRepository/HubJpaRepository.java
+++ hub-service/src/main/java/com/sparta/hubservice/infra/repository/JpaRepository/HubJpaRepository.java
@@ -3,7 +3,12 @@ package com.sparta.hubservice.infra.repository.JpaRepository;
 import com.sparta.hubservice.domain.model.Hub;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface HubJpaRepository extends JpaRepository<Hub, UUID> {
+
+    List<Hub> findAllByNameIn(List<String> names);
+
+    Boolean existsByIdAndDeletedAtIsNull(UUID hubId);
 }
