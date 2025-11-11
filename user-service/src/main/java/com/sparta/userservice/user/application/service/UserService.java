52287dd4cba815c3bab2ac520ce113e749524341
thdwnsdlf12 <il4500@naver.com>
2025-11-11 23:16:16 +0900

    feat: UserService 회원가입 로직 추가 - 검증 로직이 지나치게 많아져서 ValidDtoStrategy로 분리함

--- user-service/src/main/java/com/sparta/userservice/user/application/service/UserService.java
+++ user-service/src/main/java/com/sparta/userservice/user/application/service/UserService.java
@@ -3,6 +3,10 @@ package com.sparta.userservice.user.application.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sparta.userservice.user.application.dto.SignupUserCommand;
+import com.sparta.userservice.user.application.strategy.PasswordService;
+import com.sparta.userservice.user.application.strategy.ValidDtoStrategy;
+import com.sparta.userservice.user.domain.model.User;
 import com.sparta.userservice.user.domain.repository.UserRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -14,4 +18,22 @@ public class UserService {
 
 	private final UserRepository userRepository;
 
+	private final ValidDtoStrategy validDtoStrategy;
+	private final PasswordService passwordService;
+
+	@Transactional
+	public User signup(SignupUserCommand command) {
+
+		validDtoStrategy.validUsername(command.username());
+
+		validDtoStrategy.validateRoleSpecificData(command.role(), command.hubId(), command.companyId());
+
+		User savedUser = command.toEntity(passwordService.hashPassword(command.password()));
+
+		userRepository.save(savedUser);
+
+		validDtoStrategy.createRoleSpecificEntity(savedUser, command.hubId(), command.companyId());
+
+		return savedUser;
+	}
 }
