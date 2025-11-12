6ab4d0bc40230a71a8610edc391b104b8d133a36
JungInGyu <dltnswn20@naver.com>
2025-11-12 11:50:11 +0900

    feat: 배송 경로 엔티티

---
+++ delivery-service/src/main/java/com/sparta/deliveryservice/domain/model/DeliveryRoute.java
@@ -0,0 +1,42 @@
+package com.sparta.deliveryservice.domain.model;
+
+import com.sparta.common.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "p_delivery_route")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class DeliveryRoute extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    @Column(nullable = false)
+    private UUID departureHubId;
+
+    @Column(nullable = false)
+    private UUID arrivalHubId;
+
+    @Column(nullable = false)
+    private Integer sequence;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private DeliveryRouteStatus status;
+
+    private Double actualDistance;
+
+    private Integer actualDuration;
+
+}
