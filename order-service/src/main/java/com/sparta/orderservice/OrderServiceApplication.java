311d61958f318f4f9458fd61320e42cf1eaac910
naminsu <naminsoo1020@naver.com>
2025-11-13 14:50:07 +0900

    feat: @EnableJpaAuditing,@EnableFeignClients 어노테이션 추가

--- order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
+++ order-service/src/main/java/com/sparta/orderservice/OrderServiceApplication.java
@@ -3,13 +3,17 @@ package com.sparta.orderservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
+@EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
 public class OrderServiceApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OrderServiceApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
 
 }
