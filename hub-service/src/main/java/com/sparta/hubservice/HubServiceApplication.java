d247a39cbc6fd1998d1203d3168cd3279bbcec4f
soyoon <152174972+soyxni@users.noreply.github.com>
2025-11-13 14:40:08 +0900

    config: redis 설정

--- hub-service/src/main/java/com/sparta/hubservice/HubServiceApplication.java
+++ hub-service/src/main/java/com/sparta/hubservice/HubServiceApplication.java
@@ -2,9 +2,11 @@ package com.sparta.hubservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @EnableDiscoveryClient
 @SpringBootApplication
